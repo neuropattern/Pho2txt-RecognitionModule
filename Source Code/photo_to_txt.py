b363a20b2e06f6ff98e4163c5670280be35c3ca4
AleshaShch <36892162+aleshashch@users.noreply.github.com>
2019-04-10 14:41:28 +0300

    Enhancement text detection result(non-maximum suppression)

--- Source Code/photo_to_txt.py
+++ Source Code/photo_to_txt.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import pytesseract
 from imutils.object_detection import non_max_suppression
+from nms import (felzenszwalb, fast, malisiewicz, nms)
 from skimage.filters import (threshold_sauvola)
 
 IMAGE_WIDTH = 320
@@ -35,12 +36,15 @@ def custom_threshold(img):
     return thr_img
 
 
-def text_recognition(img, rects, confidence):
+def get_image_ratio(img):
     (height, width) = img.shape[:2]
-    ratio_w = width / float(IMAGE_WIDTH)
-    ratio_h = height / float(IMAGE_HEIGHT)
+    return width / float(IMAGE_WIDTH), height / float(IMAGE_HEIGHT)
 
-    boxes = non_max_suppression(np.array(rects), probs=confidence)
+
+def text_recognition(img, boxes):
+    (height, width) = img.shape[:2]
+    (ratio_w, ratio_h) = get_image_ratio(img)
+
     boxes = sorted(boxes, key=lambda r: r[1])
 
     text = ""
@@ -50,8 +54,8 @@ def text_recognition(img, rects, confidence):
         end_x = int(end_x * ratio_w)
         end_y = int(end_y * ratio_h)
 
-        d_x = int((end_x - start_x) * 0.01)
-        d_y = int((end_y - start_y) * 0.01)
+        d_x = int((end_x - start_x) * 0.1)
+        d_y = int((end_y - start_y) * 0.1)
 
         start_x = max(0, start_x - d_x)
         start_y = max(0, start_y - d_y)
@@ -66,6 +70,7 @@ def text_recognition(img, rects, confidence):
 
 
 def text_detection(img):
+    orig = img.copy()
     img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT))
     height, width = IMAGE_HEIGHT, IMAGE_WIDTH
 
@@ -75,14 +80,39 @@ def text_detection(img):
     net.setInput(img_blob)
     (scores, geometry) = net.forward(LAYER_NAMES)
 
+    (rects, confidences, baggage) = handle_detection_results(scores, geometry)
+
+    offsets, thetas = [], []
+    for i in baggage:
+        offsets.append(i['offset'])
+        thetas.append(i['angle'])
+
+    functions = [felzenszwalb.nms, fast.nms, malisiewicz.nms]
+
+    for i, function in enumerate(functions):
+        indicies = nms.boxes(rects, confidences, nms_function=function, confidence_threshold=MIN_CONFIDENCE,
+                             nsm_threshold=0.4)
+        indicies = np.array(indicies).reshape(-1)
+        draw_rects = np.array(rects)[indicies]
+
+        draw_box(orig, draw_rects)
+
+        #cv2.imshow("d", orig)
+        #cv2.waitKey(0)
+
+    return rects, confidences
+
+
+def handle_detection_results(scores, geometry):
     (num_row, num_col) = scores.shape[2:4]
-    rects, confidence, data = [], [], []
+    rects, confidence, baggage = [], [], []
+
     for y in range(0, num_row):
         scores_data = scores[0, 0, y]
-        data_0x = geometry[0, 0, y]
-        data_1x = geometry[0, 1, y]
-        data_2x = geometry[0, 2, y]
-        data_3x = geometry[0, 3, y]
+        data_top = geometry[0, 0, y]
+        data_right = geometry[0, 1, y]
+        data_bottom = geometry[0, 2, y]
+        data_left = geometry[0, 3, y]
         angle_data = geometry[0, 4, y]
 
         for x in range(0, num_col):
@@ -90,23 +120,41 @@ def text_detection(img):
                 continue
 
             (offset_x, offset_y) = (x * 4.0, y * 4.0)
-            angle = angle_data[x]
-            cos = np.cos(angle)
-            sin = np.sin(angle)
 
-            h = data_0x[x] + data_2x[x]
-            w = data_1x[x] + data_3x[x]
+            upper_right = (offset_x + data_right[x], offset_y - data_top[x])
+            lower_right = (offset_x + data_right[x], offset_y + data_bottom[x])
+            upper_left = (offset_x - data_left[x], offset_y - data_top[x])
+            lower_left = (offset_x - data_left[x], offset_y + data_bottom[x])
 
-            end_x = int(offset_x + (cos * data_1x[x]) + (sin * data_2x[x]))
-            end_y = int(offset_y - (sin * data_1x[x]) + (cos * data_2x[x]))
-            start_x = int(end_x - w)
-            start_y = int(end_y - h)
+            rects.append([int(upper_left[0]), int(upper_left[1]), int(lower_right[0] - upper_left[0]),
+                         int(lower_right[1] - upper_left[1])])
+            confidence.append(float(scores_data[x]))
+            baggage.append({"offset": (offset_x, offset_y), "angle": angle_data[x], "upper_right": upper_right,
+                            "lower_right": lower_right, "upper_left": upper_left, "lower_left": lower_left,
+                            "data_top": data_top[x], "data_right": data_right[x], "data_bottom": data_bottom[x],
+                            "data_left": data_left[x]})
 
-            rects.append((start_x, start_y, end_x, end_y))
-            confidence.append(scores_data[x])
-            data.clear()
+    return rects, confidence, baggage
 
-    return rects, confidence
+
+def show_boundaries(img, boxes):
+    (ratio_w, ratio_h) = get_image_ratio(img)
+    for (start_x, start_y, end_x, end_y) in boxes:
+        start_x = int(start_x * ratio_w)
+        start_y = int(start_y * ratio_h)
+        end_x = int(end_x * ratio_w)
+        end_y = int(end_y * ratio_h)
+        cv2.rectangle(img, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
+
+
+def draw_box(img, boxes):
+    (ratio_w, ratio_h) = get_image_ratio(img)
+    for (x, y, w, h) in boxes:
+        start_x = int(x * ratio_w)
+        start_y = int(y * ratio_h)
+        end_x = int((x + w) * ratio_w)
+        end_y = int((y + h) * ratio_h)
+        cv2.rectangle(img, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
 
 
 def to_txt(img_name, arg):
@@ -114,5 +162,11 @@ def to_txt(img_name, arg):
     filtration()
     binarization(img, arg)
     (rects, confidence) = text_detection(img)
-    text = text_recognition(img, rects, confidence)
+    boxes = non_max_suppression(np.array(rects), probs=confidence)
+    #show_boundaries(img, boxes)
+    #text = text_recognition(img, boxes)
+    text = "temp"
     return text
+
+
+#print(to_txt("text.png", 'ta'))
